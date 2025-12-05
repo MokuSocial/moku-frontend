6d3abb496c349e9183eed6ec836692bb29c5e31f
jantizio <emanuele0246@gmail.com>
2025-12-05 11:46:11 +0100

    feat: image fallback directive

---
+++ src/app/directives/img-fallback.spec.ts
@@ -0,0 +1,8 @@
+import { ImgFallbackDirective } from './img-fallback';
+
+describe('ImgFallback', () => {
+  it('should create an instance', () => {
+    // const directive = new ImgFallbackDirective();
+    // expect(directive).toBeTruthy();
+  });
+});
