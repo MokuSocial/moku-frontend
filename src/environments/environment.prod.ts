30ebb6974ed7960540235c45c015f123a7a2d07c
Emanuele Pischetola <47535021+jantizio@users.noreply.github.com>
2025-12-14 11:44:56 +0100

    Update gqlUri to use HTTPS in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'YOUR_API_KEY_HERE',
-  gqlUri: 'http://moku-dev.chickenkiller.com/graphql',
+  gqlUri: 'https://moku-dev.chickenkiller.com/graphql',
 };
